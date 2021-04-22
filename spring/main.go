fc06ae9db37760ed91ddcc8b4f16a8ceddc956bc
TinaLand <huihuijiang0619@gmail.com>
2021-04-22 23:09:33 +0000

    Update

--- spring/main.go
+++ spring/main.go
@@ -18,5 +18,6 @@ func main() {
     // different company or team
     // cross domain request
     r.Handle("/upload", http.HandlerFunc(uploadHandler)).Methods("POST", "OPTIONS")
+    r.Handle("/search", http.HandlerFunc(searchHandler)).Methods("GET", "OPTIONS")
     log.Fatal(http.ListenAndServe(":8080", r))
 }
